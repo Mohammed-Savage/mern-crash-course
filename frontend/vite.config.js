432d007986c69b5f062c7d6a6ce26e3a3205e5c9
Mohammed Omar <omartechmohammed@gmail.com>
2025-05-14 21:43:22 -0400

    added paths to vite config js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
+// This was the original code vite loaded with:
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// This is what I added to make it work with paths:
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
