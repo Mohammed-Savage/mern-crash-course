c8141c54de49d216cf214dec71ea4227e979b84f
Mohammed Omar <omartechmohammed@gmail.com>
2025-05-16 00:25:35 -0400

    set hard host for front end to 5k created the function to update products globally

---
+++ frontend/src/store/product.js
@@ -0,0 +1,30 @@
+// This file is to host our global state and various functions that we can use throughout our app.
+// It is a good practice to keep our global state in a separate file so that we can easily manage it.
+
+// import { useState } from "react";
+import { create } from "zustand";
+
+// This is a custom hook that we can use to access our global state.
+export const useProductStore = create((set) => ({
+    products: [],
+    setProducts: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if (!newProduct.name || !newProduct.description || !newProduct.price || !newProduct.image) {
+            return { success: false, message: "Please fill in all fields" };
+        }
+        const res = await fetch("/api/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newProduct),
+        });
+        const data = await res.json();
+        set((state) => ({
+            products: [...state.products, data.data],
+        }));
+    }
+}));
+
+// This is boilerplate local state hook.
+// const [state, setState] = useState([])
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import { ChakraProvider } from '@chakra-ui/react';
 
-// https://vite.dev/config/
-// This was the original code vite loaded with:
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// This is what I added to make it work with paths:
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  server: {
+    port: 5000, // This is the port I want to use for my app.
+    host: true, // This will allow the app to be accessed from other devices on the same network.
+    open: true, // This will open the app in the browser when the server starts.
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // This is the target server for the proxy.
+      },
     },
   },
 });
+
+
+
+
+
+// This is my first config file that I moved on from to hard bind the port to 5000.
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// });
